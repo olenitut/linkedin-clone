9ce96e6698ab4210dc5de0240bb7756036f32ac1
Ana Hordieieva <gordeevaisgreat@gmail.com>
2023-10-29 14:57:12 +0200

    add example typography

---
+++ src/components/ui/typography/typography-variants.ts
@@ -0,0 +1,23 @@
+/// TEST FOR NOW
+import { cva } from "class-variance-authority";
+
+export enum ITypographyVariants {
+  P = "p",
+  H1 = "h1",
+  // ADD MORE
+}
+
+const buttonVariants = cva(["font-semibold"], {
+  variants: {
+    intent: {
+      [ITypographyVariants.P]: ["text-sm"],
+      [ITypographyVariants.H1]: ["text-lg", "font-bold"],
+    },
+  },
+
+  defaultVariants: {
+    intent: ITypographyVariants.P,
+  },
+});
+
+export default buttonVariants;
