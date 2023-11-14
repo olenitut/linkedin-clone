1ee5c4109f028a506cc8d6e61bdc2f8c270139ac
dovbush-olha <dovbushDOV@gmail.com>
2023-11-14 18:08:25 +0200

    [update] implement sign out and header updates

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,3 +1,12 @@
+import { Container } from '@/components/ui/layouts';
+import { Typography, TypographyVariant } from '@/components/ui/typography';
+
 export default function Feed() {
-  return <div />;
+  return (
+    <Container>
+      <div className='flex flex-col items-center justify-center pt-[100px]'>
+        <Typography variant={TypographyVariant.h2}>Welcome</Typography>
+      </div>
+    </Container>
+  );
 }
--- src/app/signin/components/SignInForm.tsx
+++ src/app/signin/components/SignInForm.tsx
@@ -2,28 +2,16 @@
 
 import Link from 'next/link';
 import { FormEvent, useState } from 'react';
-import { signIn, useSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
 
 import { CustomInput } from '@/components/ui/inputs';
 import { Typography, TypographyVariant } from '@/components/ui/typography';
 import { Button, ButtonSize, ButtonVariant } from '@/components/ui/buttons';
 
-// const initialState = {
-//   issues: null,
-//   error: null,
-//   success: null,
-// };
-
-// const toastId = 'sign-in-error';
-
 export function SignInForm() {
-  //
-  // const notify = () => {
-  //   toast.error(state.error, { toastId });
-  // };
-
-  const session = useSession();
+  const router = useRouter();
 
   const [loginEmail, setLoginEmail] = useState('');
   const [loginPass, setLoginPass] = useState('');
@@ -42,63 +30,59 @@ export function SignInForm() {
         'Incorrect email or password. Please double-check your credentials and try again.'
       );
     }
+
+    if (signinResponse?.url) {
+      router.push(signinResponse.url);
+      toast.success('You are successfully signed in.');
+    }
   };
 
   return (
-    <>
-      <form className='flex w-full flex-col gap-7' onSubmit={handleLogin}>
+    <form className='flex w-full flex-col gap-7' onSubmit={handleLogin}>
+      <CustomInput
+        isRequired
+        label='Email'
+        placeholder='Email'
+        value={loginEmail}
+        onChange={(e) => setLoginEmail(e.target.value)}
+      />
+      <div className='flex flex-col items-center gap-1'>
         <CustomInput
           isRequired
-          label='Email'
-          placeholder='Email'
-          value={loginEmail}
-          onChange={(e) => setLoginEmail(e.target.value)}
+          type='password'
+          placeholder='Password'
+          label='Password'
+          value={loginPass}
+          onChange={(e) => setLoginPass(e.target.value)}
         />
-        <div className='flex flex-col items-center gap-1'>
-          <CustomInput
-            isRequired
-            type='password'
-            placeholder='Password'
-            label='Password'
-            value={loginPass}
-            onChange={(e) => setLoginPass(e.target.value)}
-          />
 
-          <Link href='/forgot-password' className='self-end'>
-            <Typography variant={TypographyVariant.Small} className='text-black-backdrop'>
-              Forgot?
-            </Typography>
-          </Link>
-        </div>
-
-        <div className='flex items-center justify-center gap-2'>
-          <Typography variant={TypographyVariant.Small} className='opacity-70'>
-            Don&apos;t have an account?
+        <Link href='/forgot-password' className='self-end'>
+          <Typography variant={TypographyVariant.Small} className='text-black-backdrop'>
+            Forgot?
           </Typography>
-          <Link
-            href='/signup'
-            className='text-red-900 transition-colors delay-150 hover:text-red-700 '
-          >
-            <Typography variant={TypographyVariant.Small}>Sign Up</Typography>
-          </Link>
-        </div>
+        </Link>
+      </div>
 
-        <Button
-          // isDisabled={pending}
-          // aria-disabled={pending}
-          type='submit'
-          variant={ButtonVariant.Primary}
-          size={ButtonSize.Full}
-          className='tracking-wide disabled:cursor-default disabled:opacity-50'
+      <div className='flex items-center justify-center gap-2'>
+        <Typography variant={TypographyVariant.Small} className='opacity-70'>
+          Don&apos;t have an account?
+        </Typography>
+        <Link
+          href='/signup'
+          className='text-red-900 transition-colors delay-150 hover:text-red-700 '
         >
-          Login
-        </Button>
-      </form>
+          <Typography variant={TypographyVariant.Small}>Sign Up</Typography>
+        </Link>
+      </div>
 
-      <p>{session?.data?.user?.name}</p>
-      <p>{session?.data?.user?.email}</p>
-
-      {/* {state.error && notify()} */}
-    </>
+      <Button
+        type='submit'
+        variant={ButtonVariant.Primary}
+        size={ButtonSize.Full}
+        className='tracking-wide disabled:cursor-default disabled:opacity-50'
+      >
+        Login
+      </Button>
+    </form>
   );
 }
