f4a456b024193e96636930da268a89db43362288
dovbush-olha <dovbushDOV@gmail.com>
2023-11-15 17:10:41 +0200

    [update] delete unused div in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={spaceGrotesk.className}>
-        <div id='overlay' />
         <AuthProvider>
           <Header />
           {children}
